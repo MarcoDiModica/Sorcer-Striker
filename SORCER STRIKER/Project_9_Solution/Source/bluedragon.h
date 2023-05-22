8ea35acc8c6c5c6a4e82d37f359f76e6d87ed25a
PabloL2003 <pablol.seda@gmail.com>
2023-05-22 10:53:39 +0200

    a

--- SORCER STRIKER/Project_9_Solution/Source/bluedragon.h
+++ SORCER STRIKER/Project_9_Solution/Source/bluedragon.h
@@ -1,41 +1,41 @@
-#ifndef __ENEMY_BDRAGON_H__
-#define __ENEMY_BDRAGONK_H__
-
-#include "Enemy.h"
-#include "Path.h"
-#include "ModuleParticles.h"
-/*
-
-class Enemy_Bdragon :public Enemy {
-public:
-
-	Enemy_Bdragon(int x, int y);
-
-	void Update();
-
-private:
-	// A set of steps that define the position in the screen
-	// And an animation for each step
-	Path path;
-
-	int yoldi;
-
-	float waveRatio = 0.9f;
-
-	// The speed at which the wave ratio is increased
-	float waveRatioSpeed = 0.03f;
-
-	// The original spawning position. The wave will be calculated from that
-	int spawn_y = 0;
-
-	// The total height of the wave
-	int waveHeight = 50;
-
-	// This enemy has one sprite and one frame
-	// We are keeping it an animation for consistency with other enemies
-	Animation flyAnim;
-	Animation turnAnim1;
-	Animation turnAnim2;
-	Animation turnAnim3;
-	Animation turnAnim4;
-}
+//#ifndef __ENEMY_BDRAGON_H__
+//#define __ENEMY_BDRAGONK_H__
+//
+//#include "Enemy.h"
+//#include "Path.h"
+//#include "ModuleParticles.h"
+///*
+//
+//class Enemy_Bdragon :public Enemy {
+//public:
+//
+//	Enemy_Bdragon(int x, int y);
+//
+//	void Update();
+//
+//private:
+//	 A set of steps that define the position in the screen
+//	 And an animation for each step
+//	Path path;
+//
+//	int yoldi;
+//
+//	float waveRatio = 0.9f;
+//
+//	 The speed at which the wave ratio is increased
+//	float waveRatioSpeed = 0.03f;
+//
+//	 The original spawning position. The wave will be calculated from that
+//	int spawn_y = 0;
+//
+//	 The total height of the wave
+//	int waveHeight = 50;
+//
+//	 This enemy has one sprite and one frame
+//	 We are keeping it an animation for consistency with other enemies
+//	Animation flyAnim;
+//	Animation turnAnim1;
+//	Animation turnAnim2;
+//	Animation turnAnim3;
+//	Animation turnAnim4;
+//}
