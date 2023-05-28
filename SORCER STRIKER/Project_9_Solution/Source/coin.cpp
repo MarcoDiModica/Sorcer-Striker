11b448950f5fd3a08dbc0fe47781986a9a93b662
Yeeway69 <yeyiwei1@gmail.com>
2023-05-28 15:57:43 +0200

    Fixed Coin animation cuts

--- SORCER STRIKER/Project_9_Solution/Source/coin.cpp
+++ SORCER STRIKER/Project_9_Solution/Source/coin.cpp
@@ -12,12 +12,12 @@ Coin::Coin(int x, int y) : Enemy(x, y)
 	cnt = 1;
 	tipo = Enemy_Type::COIN;
 
-	hoverAnim.PushBack({ 22,377,11,11 });
-	hoverAnim.PushBack({ 44, 379, 11, 11 });
-	hoverAnim.PushBack({ 66, 379, 11, 11});
-	hoverAnim.PushBack({ 82, 379, 11, 11 });
-	hoverAnim.PushBack({ 99, 379, 11, 11 });
-	hoverAnim.PushBack({ 115, 379, 11, 11 });
+	hoverAnim.PushBack({ 22,377,15,15 });
+	hoverAnim.PushBack({ 44, 379, 15, 15 });
+	hoverAnim.PushBack({ 66, 379, 15, 15});
+	hoverAnim.PushBack({ 82, 379, 15, 15 });
+	hoverAnim.PushBack({ 99, 379, 15, 15 });
+	hoverAnim.PushBack({ 115, 379, 15, 15 });
 	hoverAnim.speed = 0.2f;
 	hoverAnim.loop = true;
 
