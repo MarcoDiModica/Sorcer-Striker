196882662565ba1b89be1e68c8de26a8895f201d
Marco Di Modica Elias <marcodimodica@protonmail.com>
2023-05-29 00:42:53 +0200

    fhfthhtf
    thfhftfhgh

--- SORCER STRIKER/Project_9_Solution/Source/Enemy_BrownShip.cpp
+++ SORCER STRIKER/Project_9_Solution/Source/Enemy_BrownShip.cpp
@@ -11,29 +11,29 @@ Enemy_BrownShip::Enemy_BrownShip(int x, int y) : Enemy(x, y)
 	cnt = 3;
 	tipo = Enemy_Type::BROWNSHIP;
 
-	// Define the fly animation
-	flyAnim.PushBack({68, 0, 34, 31});
+	flyAnim.PushBack({ 44, 5, 36, 34 });
 	
-	// Define the turning animation
-	turnAnim1.PushBack({ 109, 0, 34, 31});
+	turnAnim1.PushBack({ 231, 3, 36, 34});
 
-	turnAnim2.PushBack({ 144, 0, 34, 31 });
-	
-	turnAnim3.PushBack({ 179, 0, 34, 31 });
-	
-	turnAnim4.PushBack({ 215, 0, 34, 31 });
-	 
-	// Define the movement path
-	path.PushBack({ 0.0f, -0.3f }, 200, &flyAnim);
-	currentAnim = path.GetCurrentAnimation();
-	path.PushBack({ 0.2f, -0.5f }, 60, &turnAnim1);
-	currentAnim = path.GetCurrentAnimation();
-	path.PushBack({ 0.3f, -0.6f }, 60, &turnAnim2);
-	currentAnim = path.GetCurrentAnimation();
-	path.PushBack({ 0.4f, -2.0f }, 60, &turnAnim3);
-	currentAnim = path.GetCurrentAnimation();
-	path.PushBack({ 0.0f, -0.3f }, 1000, &turnAnim4);
-	currentAnim = path.GetCurrentAnimation();
+	currentAnim = &flyAnim;
+
+	//turnAnim2.PushBack({ 144, 0, 34, 31 });
+	//
+	//turnAnim3.PushBack({ 179, 0, 34, 31 });
+	//
+	//turnAnim4.PushBack({ 215, 0, 34, 31 });
+	// 
+	//// Define the movement path
+	//path.PushBack({ 0.0f, -0.3f }, 200, &flyAnim);
+	//currentAnim = path.GetCurrentAnimation();
+	//path.PushBack({ 0.2f, -0.5f }, 60, &turnAnim1);
+	//currentAnim = path.GetCurrentAnimation();
+	//path.PushBack({ 0.3f, -0.6f }, 60, &turnAnim2);
+	//currentAnim = path.GetCurrentAnimation();
+	//path.PushBack({ 0.4f, -2.0f }, 60, &turnAnim3);
+	//currentAnim = path.GetCurrentAnimation();
+	//path.PushBack({ 0.0f, -0.3f }, 1000, &turnAnim4);
+	//currentAnim = path.GetCurrentAnimation();
 
 	collider = App->collisions->AddCollider({ position.y + 5, position.y, 34, 30}, Collider::Type::ENEMY, (Module*)App->enemies);
 }
@@ -58,7 +58,7 @@ void Enemy_BrownShip::Update()
 
 	if (position.y > (App->player->OPTMIZENELJUEGUITO + 30)) {
 		position.y -= 3;
-	}
+	} 
 
 	Enemy::Update();
 }
